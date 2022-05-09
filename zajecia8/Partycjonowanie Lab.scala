1afd3f101ec431ca952ae8f05d3c84c80d3d929a
DDefinski <101058743+DDefinski@users.noreply.github.com>
2022-05-10 00:54:17 +0200

    Add files via upload

---
+++ zajecia8/Partycjonowanie Lab.scala
@@ -0,0 +1,80 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## Jak działa partycjonowanie
+// MAGIC 
+// MAGIC 1. Rozpocznij z 8 partycjami.
+// MAGIC 2. Uruchom kod.
+// MAGIC 3. Otwórz **Spark UI**
+// MAGIC 4. Sprawdź drugi job (czy są jakieś różnice pomięczy drugim)
+// MAGIC 5. Sprawdź **Event Timeline**
+// MAGIC 6. Sprawdzaj czas wykonania.
+// MAGIC   * Uruchom kilka razy rzeby sprawdzić średni czas wykonania.
+// MAGIC 
+// MAGIC Powtórz z inną liczbą partycji
+// MAGIC * 1 partycja
+// MAGIC * 7 partycja
+// MAGIC * 9 partycja
+// MAGIC * 16 partycja
+// MAGIC * 24 partycja
+// MAGIC * 96 partycja
+// MAGIC * 200 partycja
+// MAGIC * 4000 partycja
+// MAGIC 
+// MAGIC Zastąp `repartition(n)` z `coalesce(n)` używając:
+// MAGIC * 6 partycji
+// MAGIC * 5 partycji
+// MAGIC * 4 partycji
+// MAGIC * 3 partycji
+// MAGIC * 2 partycji
+// MAGIC * 1 partycji
+// MAGIC 
+// MAGIC ** *Note:* ** *Dane muszą być wystarczająco duże żeby zaobserwować duże różnice z małymi partycjami.*<br/>* To co możesz sprawdzić jak zachowują się małe dane z dużą ilośćia partycji.*
+
+// COMMAND ----------
+
+// val slots = sc.defaultParallelism
+spark.conf.get("spark.sql.shuffle.partitions")
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+val schema = StructType(
+  List(
+    StructField("timestamp", StringType, false),
+    StructField("site", StringType, false),
+    StructField("requests", IntegerType, false)
+  )
+)
+
+val data = spark.read
+  .option("header", "true")
+  .option("sep", "\t")
+  .schema(schema)
+  .csv("dbfs:/FileStore/tables/pageviews_by_second.tsv")
+
+data.write.mode("overwrite").parquet("dbfs:/pageviews_by_second.parquet")
+
+
+// COMMAND ----------
+
+spark.catalog.clearCache()
+
+val df = spark.read
+  .parquet("dbfs:/pageviews_by_second.parquet")
+  .repartition(2000)
+//  .coalesce(6)
+  .groupBy("site").sum()
+
+
+df.explain
+df.count()
+
+// COMMAND ----------
+
+df.rdd.getNumPartitions
+
+// COMMAND ----------
+
+
