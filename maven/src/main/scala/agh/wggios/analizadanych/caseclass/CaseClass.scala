1b554b7c18d8406b20e2a909ee52112084859ac1
DDefinski <101058743+DDefinski@users.noreply.github.com>
2022-06-19 23:37:00 +0200

    Delete maven directory

--- maven/src/main/scala/agh/wggios/analizadanych/caseclass/CaseClass.scala
+++
@@ -1,6 +0,0 @@
-package agh.wggios.analizadanych.caseclass
-
-//case class FlightCC(DEST_COUNTRY_NAME: String, ORIGIN_COUNTRY_NAME: String, count: Int)
-
-case class person(imdb_title_id: String, ordering: Int, imdb_name_id: String,category: String,job: String, characters: String*)
-
--- maven/src/main/scala/agh/wggios/analizadanych/session/sparksession.scala
+++
@@ -1,6 +0,0 @@
-package agh.wggios.analizadanych.session
-import org.apache.spark.sql.SparkSession
-
-class sparksession {
-  val spark: SparkSession = SparkSession.builder().config("spark.master", "local").getOrCreate()
-}
